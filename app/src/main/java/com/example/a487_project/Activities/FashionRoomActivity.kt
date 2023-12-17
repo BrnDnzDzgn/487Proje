4e5d4a6d041bba0cdeded825cfcc18f307c3c605
kamilaalv <kamilaaliyeva58@gmail.com>
2023-12-17 21:21:12 +0300

    ZULAAAAAAL IS SLAYING

--- app/src/main/java/com/example/a487_project/Activities/FashionRoomActivity.kt
+++ app/src/main/java/com/example/a487_project/Activities/FashionRoomActivity.kt
@@ -3,9 +3,15 @@ package com.example.a487_project.Activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.a487_project.databinding.ActivityFashionRoomBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.example.a487_project.R
+import com.example.a487_project.RecyclerViewPackage.CustomRecyclerViewAdapter
+import com.example.a487_project.Classes.Category
 
 class FashionRoomActivity : AppCompatActivity() { //Kamila
     lateinit var binding: ActivityFashionRoomBinding
+    lateinit var recyclerView: RecyclerView
+    lateinit var adapter: CustomRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,9 +19,25 @@ class FashionRoomActivity : AppCompatActivity() { //Kamila
         binding=ActivityFashionRoomBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        recyclerView = binding.recyclerView
+
+        // Add categories "A," "B," and "C" to yourCategoryList
+        val categoryList: ArrayList<com.example.a487_project.Classes.Category> = arrayListOf(
+            com.example.a487_project.Classes.Category("A"),
+            com.example.a487_project.Classes.Category("B"),
+            com.example.a487_project.Classes.Category("C"),
+            com.example.a487_project.Classes.Category("D"),
+        )
+
+        adapter = CustomRecyclerViewAdapter(this, categoryList)
+        adapter.setHorizontalLayoutManager(recyclerView)
+        recyclerView.adapter = adapter
+
         binding.goMain.setOnClickListener {
             finish()
         }
+
+
 
     }
 }
