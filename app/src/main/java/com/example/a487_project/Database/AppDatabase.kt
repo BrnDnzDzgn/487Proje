d5f6e65bba3e56ecbde6624bab79117a1f63eaee
Beyza  Özbay <beyzaozbay00@gmail.com>
2023-12-25 17:58:04 +0300

    not complete

--- app/src/main/java/com/example/a487_project/Database/AppDatabase.kt
+++ app/src/main/java/com/example/a487_project/Database/AppDatabase.kt
@@ -4,9 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.a487_project.Classes.ClothingItemKami
 
-@Database(entities = [ClothingItemKami::class], version = 1, exportSchema = false)
+@Database(entities = [ClothingItemEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun clothingItemDao(): ClothingItemDao
@@ -21,7 +20,8 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "app_database"
-                ).build()
+                ).fallbackToDestructiveMigration() // This line allows destructive migration, modify as per your needs
+                    .build()
                 INSTANCE = instance
                 instance
             }
--- app/src/main/java/com/example/a487_project/Database/ClothingItemDao.kt
+++ app/src/main/java/com/example/a487_project/Database/ClothingItemDao.kt
@@ -1,10 +1,11 @@
+// File: ClothingItemDao.kt
 package com.example.a487_project.Database
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.example.a487_project.Classes.ClothingItemKami
 
 @Dao
 interface ClothingItemDao {
@@ -12,6 +13,15 @@ interface ClothingItemDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertClothingItem(clothingItem: ClothingItemEntity)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllClothingItems(clothingItems: List<ClothingItemEntity>)
+
+    @Query("SELECT * FROM clothing_items")
+    suspend fun getAllClothingItems(): List<ClothingItemEntity>
+
     @Query("SELECT * FROM clothing_items WHERE category = :category")
-    fun getClothingItemsByCategory(category: String): LiveData<List<ClothingItemEntity>>
+    suspend fun getClothingItemsByCategory(category: String): List<ClothingItemEntity>
+
+    @Query("DELETE FROM clothing_items")
+    suspend fun clearAllClothingItems()
 }
