d1ebbb3451ddbcc0e63acd2ca92c9955c7c92b2d
Boran Deniz Düzgün <>
2023-12-27 22:42:32 +0300

    everything is working

--- app/src/main/java/com/example/a487_project/Activities/MainActivity.kt
+++ app/src/main/java/com/example/a487_project/Activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a487_project.Activities
 
+import android.app.NotificationManager
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -8,6 +9,10 @@ import android.widget.ImageView
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.Constraints
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.example.a487_project.Classes.CustomWorker
 import com.example.a487_project.Classes.MusicPlayer
 import com.example.a487_project.Classes.ThemeSys
 import com.example.a487_project.Classes.Themes
@@ -17,6 +22,12 @@ import com.example.a487_project.databinding.ActivityMainBinding
 import com.nineoldandroids.animation.AnimatorListenerAdapter
 import com.nineoldandroids.animation.AnimatorSet
 import com.nineoldandroids.animation.ObjectAnimator
+import androidx.work.BackoffPolicy
+import androidx.work.Data
+import androidx.work.WorkInfo
+import java.util.concurrent.TimeUnit
+import androidx.lifecycle.Observer
+import androidx.work.OneTimeWorkRequestBuilder
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -24,10 +35,62 @@ class MainActivity : AppCompatActivity() {
     lateinit var spinner: Spinner
     var pos = 0
 
+    // for worker that plays music
+    lateinit var workManager: WorkManager
+    lateinit var workRequest: OneTimeWorkRequest
+    lateinit var customWorker: CustomWorker
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //initialize mediaplayer with the background music and restart it after it ends
+        val mNotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        val intent = intent
+        val notificationId = intent.getIntExtra("notificationID", 0)
+        val message = intent.getStringExtra("msg")
+        if (message != null) {
+            mNotificationManager.cancel(notificationId)
+            //binding.tvResult.text =message
+            mNotificationManager.cancel(notificationId)
+        }
+
+        val constraints: Constraints = Constraints.Builder()
+            .build()
+
+        workRequest = OneTimeWorkRequest.Builder(CustomWorker::class.java)
+            .setInputData(Data.Builder().putInt("num", 10).putString("boran", "düzgün").build())
+            .build()
+
+        workRequest = OneTimeWorkRequestBuilder<CustomWorker>().setBackoffCriteria(
+            BackoffPolicy.LINEAR,
+            30,
+            TimeUnit.MILLISECONDS)
+            .build()
+
+        workManager = WorkManager.getInstance(this)
+
+        binding.textView.setOnClickListener(View.OnClickListener {
+
+            workRequest = OneTimeWorkRequest.Builder(CustomWorker::class.java)
+                .setInputData(Data.Builder().putInt("num", 10).putString("boran", "düzgün").build())
+                .build()
+
+            workManager.enqueue(workRequest)
+            Toast.makeText(this@MainActivity, "Worker is working, brain is braining",Toast.LENGTH_SHORT).show()
+
+            workManager.getWorkInfoByIdLiveData(workRequest.id).observe(this@MainActivity,
+                Observer{ workInfo ->
+                    if (workInfo != null && workInfo.state == WorkInfo.State.SUCCEEDED) {
+                        val resultData: Data = workInfo.outputData//get output of worker
+                        Toast.makeText(this@MainActivity, "SUCCEESS " ,Toast.LENGTH_LONG ).show()
+                    }
+                })
+
+        })
+
 
         ThemeSys.prepare()
         themeItems = ThemeSys.themesList
@@ -63,6 +126,11 @@ class MainActivity : AppCompatActivity() {
                 displayToast("Nothing selected")
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
     }
 
     private fun getImageResourceForTheme(themes: Themes): Int {
